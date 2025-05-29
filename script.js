385f4f30516cf985e826f0b45b6d3610c43a99b2
redcirle <ifixit@qq.com>
2025-05-29 16:49:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const tagline = document.getElementById("tagline");
+
+const charArray = [
+  "R", "e", "d", " ",
+  "<span style='color:#FF2D55;'>", "C", "i", "r", "c", "l", "e", "</span>", " ",
+  "J", "o", "k", "e", "r", " ",
+  "未", "赢", "够"
+];
+
+let index = 0;
+
+function typeWriter() {
+  if (index < charArray.length) {
+    tagline.innerHTML += charArray[index];
+    index++;
+    setTimeout(typeWriter, 200);
+  } else {
+    setTimeout(() => {
+      tagline.innerHTML = "";
+      index = 0;
+      typeWriter();
+    }, 2000);
+  }
+}
+
+window.onload = () => {
+  typeWriter();
+
+  const swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 30,
+      stretch: 0,
+      depth: 150,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+};
+function toggleQR() {
+  const popup = document.getElementById("qr-popup");
+  popup.style.display = popup.style.display === "flex" ? "none" : "flex";
+}
